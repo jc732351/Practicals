fa2582342bd67ec928e5f3ccaa44b9814ee9a6c8
jc732351 <68983419+jc732351@users.noreply.github.com>
2020-08-05 13:29:30 +0800

    prac3
    use functions

--- broken_score.py
+++ broken_score.py
@@ -1,16 +1,23 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
 
-# TODO: Fix this!
+def main():
+    score = float(input("Enter score: "))
+    get_score(score)
+    random_score(score)
 
-score = float(input("Enter score: "))
-if 0<score<50:
-    print("Bad")
-elif 50<=score<90:
-    print("Passable")
-elif 90<=score<=100:
-    print("Excellent")
-else:
-    print("Invalid score")
+def get_score(score):
+        print("Bad")
+    elif 50 <= score < 90:
+        print("Passable")
+    elif 90 <= score <= 100:
+        print("Excellent")
+    else:
+        print("Invalid score")
+    return score
+
+
+from random import randint
+def random_score(score):
+    score=randint(1,100)
+    print(score)
+
+main()
